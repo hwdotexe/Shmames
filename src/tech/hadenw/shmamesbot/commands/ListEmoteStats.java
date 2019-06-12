177ec11298938fb32472ed38dcccd3a4eea1ba12
Haden Watne <hadengw@gmail.com>
2019-06-11 21:19:13 -0600

    Sort emotes

--- src/tech/hadenw/shmamesbot/commands/ListEmoteStats.java
+++ src/tech/hadenw/shmamesbot/commands/ListEmoteStats.java
@@ -1,5 +1,10 @@
 package tech.hadenw.shmamesbot.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 import net.dv8tion.jda.core.entities.Emote;
@@ -22,14 +27,14 @@ public class ListEmoteStats implements ICommand {
 	@Override
 	public String run(String args, User author, Message message) {
 		Brain b = Shmames.getBrains().getBrain(message.getGuild().getId());
-		
 		String stats = "**Thus sayeth the Shmames:**\n";
+		LinkedHashMap<String, Integer> emotes = sortHashMapByValues(b.getEmoteStats());
 		
 		// TODO does not sort
-		if(b.getEmoteStats().keySet().size() > 0) {
+		if(emotes.keySet().size() > 0) {
 			int i = 0;
 			
-			for(String em : b.getEmoteStats().keySet()) {
+			for(String em : emotes.keySet()) {
 				List<Emote> ems = message.getGuild().getEmotesByName(em, false);
 				
 				if(!ems.isEmpty()) {
@@ -40,7 +45,7 @@ public class ListEmoteStats implements ICommand {
 						i = 1;
 					}
 					
-					stats += ems.get(0).getAsMention() + ": " + b.getEmoteStats().get(em)+"  ";
+					stats += ems.get(0).getAsMention() + ": " + emotes.get(em)+"  ";
 				}
 			}
 		}else {
@@ -48,6 +53,35 @@ public class ListEmoteStats implements ICommand {
 		}
 
 		return stats;
+	}
+	
+	// From https://stackoverflow.com/questions/8119366/sorting-hashmap-by-values
+	private LinkedHashMap<String, Integer> sortHashMapByValues(HashMap<String, Integer> passedMap) {
+	    List<String> mapKeys = new ArrayList<>(passedMap.keySet());
+	    List<Integer> mapValues = new ArrayList<>(passedMap.values());
+	    Collections.sort(mapValues);
+	    Collections.sort(mapKeys);
+
+	    LinkedHashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+
+	    Iterator<Integer> valueIt = mapValues.iterator();
+	    while (valueIt.hasNext()) {
+	        int val = valueIt.next();
+	        Iterator<String> keyIt = mapKeys.iterator();
+
+	        while (keyIt.hasNext()) {
+	            String key = keyIt.next();
+	            int comp1 = passedMap.get(key);
+	            int comp2 = val;
+
+	            if (comp1 == comp2) {
+	                keyIt.remove();
+	                sortedMap.put(key, val);
+	                break;
+	            }
+	        }
+	    }
+	    return sortedMap;
 	}
 
 	@Override
