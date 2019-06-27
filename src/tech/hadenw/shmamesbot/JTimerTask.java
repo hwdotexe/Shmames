180c46a4cdab2f8bb6d075c44d1ba869f5280550
Cynicalvoe <cynicalvoe@gmail.com>
2019-06-27 14:15:43 -0600

    Timer now supports more options

--- src/tech/hadenw/shmamesbot/JTimerTask.java
+++ src/tech/hadenw/shmamesbot/JTimerTask.java
@@ -12,11 +12,27 @@ public class JTimerTask extends TimerTask{
 	private User u;
 	private MessageChannel msgch;
 	
-	public JTimerTask(User user, MessageChannel ch, int minutes) {
+	public JTimerTask(User user, MessageChannel ch, int time, int interval) {
 		Calendar c = Calendar.getInstance();
 		Timer t = new Timer();
     	c.setTime(new Date());
-    	c.add(Calendar.MINUTE, minutes);
+    	
+    	switch(interval) {
+    	case 1:
+    		c.add(Calendar.HOUR, 24*time);
+    		break;
+    	case 2:
+    		c.add(Calendar.HOUR, time);
+    		break;
+    	case 3:
+    		c.add(Calendar.MINUTE, time);
+    		break;
+    	case 4:
+    		c.add(Calendar.SECOND, time);
+    		break;
+    	default:
+    		c.add(Calendar.SECOND, 5);
+    	}
     	
     	u = user;
     	msgch = ch;
--- src/tech/hadenw/shmamesbot/commands/JTimer.java
+++ src/tech/hadenw/shmamesbot/commands/JTimer.java
@@ -15,17 +15,47 @@ public class JTimer implements ICommand {
 	
 	@Override
 	public String getUsage() {
-		return "timer <minutes>";
+		return "timer <time>[d/h/m/s]";
 	}
 
 	@Override
 	public String run(String args, User author, Message message) {
-		if(Pattern.compile("^\\d{1,2}$").matcher(args).matches()) {
-			int minutes = Integer.parseInt(args);
+		if(Pattern.compile("^\\d{1,3}[dhms]?$").matcher(args).matches()) {
+			int time;
 			
-			new JTimerTask(author, message.getChannel(), minutes);
+			System.out.println(args.charAt(args.length()-1));
 			
-			return "Started a new :alarm_clock: for **"+minutes+"** minutes.";
+			if(isInt(args.charAt(args.length()-1))){
+				time = Integer.parseInt(args);
+			}else {
+				time = Integer.parseInt(args.substring(0, args.length()-1));
+			}
+			
+			if(time > 0) {
+				int interval = -1;
+				switch(args.charAt(args.length()-1)) {
+				case 'd':
+					interval = 1;
+					break;
+				case 'h':
+					interval = 2;
+					break;
+				case 'm':
+					interval = 3;
+					break;
+				case 's':
+					interval = 4;
+					break;
+				default:
+					interval = 3;
+				}
+				
+				new JTimerTask(author, message.getChannel(), time, interval);
+				
+				return "Started a new :alarm_clock: for **"+time+"** "+(interval==1?"Days":interval==2?"Hours":interval==3?"Minutes":interval==4?"Seconds":"");
+			}else {
+				return "Please include an actual amount of time!";
+			}
 		}else {
 			return Errors.formatUsage(Errors.WRONG_USAGE, getUsage());
 		}
@@ -44,5 +74,14 @@ public class JTimer implements ICommand {
 	@Override
 	public boolean requiresGuild() {
 		return false;
+	}
+	
+	private boolean isInt(char c) {
+		try {
+			Integer.parseInt(String.valueOf(c));
+			return true;
+		}catch(Exception e) {
+			return false;
+		}
 	}
 }
