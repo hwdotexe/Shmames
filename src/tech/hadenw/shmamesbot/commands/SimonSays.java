54bc84577eec07997fb66889ae651a7738e1c81f
Cynicalvoe <cynicalvoe@gmail.com>
2019-08-29 12:19:29 -0600

    Global emote passport; bug fixes

--- src/tech/hadenw/shmamesbot/commands/SimonSays.java
+++ src/tech/hadenw/shmamesbot/commands/SimonSays.java
@@ -1,13 +1,16 @@
 package tech.hadenw.shmamesbot.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import net.dv8tion.jda.core.entities.Emote;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
 import tech.hadenw.shmamesbot.Errors;
+import tech.hadenw.shmamesbot.Shmames;
 
 public class SimonSays implements ICommand {
 	@Override
@@ -27,14 +30,29 @@ public class SimonSays implements ICommand {
 				message.delete().complete();
 			} catch(Exception e) { }
 			
+			
+			// Try to scan all servers for the emote?
+			// :emotename:   =>   <a:emoteName:1234567890>
 			Matcher m = Pattern.compile("\\:([\\w\\d]+)\\:").matcher(args);
+			
+			// Add a space at the end so we can regex correctly
+			args += " ";
 			
 			while(m.find()) {
 				String eName = m.group(1);
-				List<Emote> e = message.getGuild().getEmotesByName(eName, true);
+				List<Emote> e = new ArrayList<Emote>();
+				
+				for(Guild g : Shmames.getJDA().getGuilds()) {
+					List<Emote> em = g.getEmotesByName(eName, true);
+					
+					if(em.size() > 0) {
+						e.add(em.get(0));
+						break;
+					}
+				}
 				
 				if(e.size() > 0) {
-					args = args.replace(":"+eName+":", e.get(0).getAsMention());
+					args = args.replace(":"+eName+": ", e.get(0).getAsMention());
 				}
 			}
 			
