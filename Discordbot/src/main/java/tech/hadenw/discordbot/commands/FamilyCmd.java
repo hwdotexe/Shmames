890a5782e7748bc20344d14a08448430666ddfb2
Cynicalvoe <hadengw@gmail.com>
2020-05-06 09:22:35 -0600

    Can now remove servers from a family

--- Discordbot/src/main/java/tech/hadenw/discordbot/commands/FamilyCmd.java
+++ Discordbot/src/main/java/tech/hadenw/discordbot/commands/FamilyCmd.java
@@ -10,6 +10,7 @@ import tech.hadenw.discordbot.Utils;
 import tech.hadenw.discordbot.storage.Brain;
 import tech.hadenw.discordbot.storage.Family;
 
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -22,18 +23,16 @@ public class FamilyCmd implements ICommand {
 	
 	@Override
 	public String getUsage() {
-		return "family <create|add|view|remove> [name|code]";
+		return "family <create|add|view|remove> [family|code] [server]";
 	}
 
 	@Override
 	public String run(String args, User author, Message message) {
-		Matcher m = Pattern.compile("^((create)|(add)|(view)|(remove))( [a-z0-9\\-]+)?$", Pattern.CASE_INSENSITIVE).matcher(args);
+		Matcher m = Pattern.compile("^((create)|(add)|(view)|(remove))( [a-z0-9\\-]+)?( \\d+)?$", Pattern.CASE_INSENSITIVE).matcher(args);
 
 		//g1 - command
 		//g6 - args
-
-		// The Family object has an ID
-		// Each server will record a list of IDs it belongs to
+		//g7 - args2
 
 		if(m.find()){
 			switch(m.group(1).toLowerCase()){
@@ -59,7 +58,8 @@ public class FamilyCmd implements ICommand {
 
 							return "The Family was created! Now let's go add other servers!.";
 						} else {
-							return Errors.formatUsage(Errors.WRONG_USAGE, getUsage());
+							return "Use `family create <familyName>` to start a new Family using this server.\n" +
+									"Please use only letters, numbers, and dashes in the name.";
 						}
 					}else{
 						return Errors.NO_PERMISSION_USER;
@@ -81,13 +81,17 @@ public class FamilyCmd implements ICommand {
 								f.clearCode();
 
 								if(message.getMember().hasPermission(Permission.ADMINISTRATOR) || Shmames.isDebug) {
-									if(!b.getFamilies().contains(f.getFamID())){
-										f.addToFamily(message.getGuild());
-										b.getFamilies().add(f.getFamID());
+									if(b.getFamilies().size() < 3) {
+										if (!b.getFamilies().contains(f.getFamID())) {
+											f.addToFamily(message.getGuild());
+											b.getFamilies().add(f.getFamID());
 
-										return "Added **"+message.getGuild().getName()+"** to the **"+f.getFamName()+"** Family!";
+											return "Added **" + message.getGuild().getName() + "** to the **" + f.getFamName() + "** Family!";
+										} else {
+											return "This server is already a member of **" + f.getFamName() + "**!\n(The code has been invalidated for security purposes)";
+										}
 									}else{
-										return "This server is already a member of **"+f.getFamName()+"**!\n(The code has been invalidated for security purposes)";
+										return "You can only join up to 3 families!\n(The code has been invalidated for security purposes)";
 									}
 								}else{
 									return Errors.NO_PERMISSION_USER+"\n(The code has been invalidated for security purposes)";
@@ -111,17 +115,23 @@ public class FamilyCmd implements ICommand {
 								sb.append(f.getFamName());
 								sb.append("\" Family contains the following servers:**");
 
-								for(long g : f.getMemberGuilds()){
+								for(long g : new ArrayList<Long>(f.getMemberGuilds())){
 									Guild guild = Shmames.getJDA().getGuildById(g);
 
 									// Quick null check!
 									if(guild == null){
 										f.getMemberGuilds().remove(g);
-										break;
+										continue;
 									}
 
-									sb.append("\n> ");
+									sb.append("\n> **");
+									sb.append(f.getMemberGuilds().indexOf(g)+1);
+									sb.append("**: ");
 									sb.append(guild.getName());
+								}
+
+								if(sb.length() == 3) {
+									sb.append("_This Family does not contain any servers._");
 								}
 
 								return sb.toString();
@@ -146,15 +156,67 @@ public class FamilyCmd implements ICommand {
 								}
 							}
 
+							if(sb.length() == 1) {
+								sb.append("_This server does not belong to a Family._");
+							}
+
 							return sb.toString();
 						}else{
 							return Errors.NO_PERMISSION_USER;
 						}
 					}
-				case "remove":
-					// If no args, assume this server. User must be an Admin.
-					// If an arg, assume another server. User must be the family owner.
-					return "Command under construction!";
+				case "remove": // <family> [server]
+					if(m.group(6) != null) {
+						String name = m.group(6).trim().toLowerCase();
+
+						if (m.group(7) != null) {
+							int server = Integer.parseInt(m.group(7).trim());
+
+							for (Family f : Shmames.getBrains().getMotherBrain().getServerFamilies()) {
+								if (f.getFamilyOwner() == author.getIdLong() && f.getFamName().equalsIgnoreCase(name)) {
+									if (f.getMemberGuilds().size() >= server) {
+										Guild g = Shmames.getJDA().getGuildById(f.getMemberGuilds().get(server));
+
+										if(g == null){
+											// Just remove it
+										}
+
+										Brain b = Shmames.getBrains().getBrain(g.getId());
+										b.getFamilies().remove(f.getFamID());
+										f.getMemberGuilds().remove(g.getIdLong());
+
+										return "Removed **"+g.getName()+"** from the **"+f.getFamName()+"** Family.";
+									} else {
+										return "That server doesn't exist in the Family!";
+									}
+								}
+							}
+
+							return Errors.NOT_FOUND;
+						} else {
+							if(message.getMember().hasPermission(Permission.ADMINISTRATOR) || Shmames.isDebug) {
+								for (Family f : Shmames.getBrains().getMotherBrain().getServerFamilies()) {
+									if(f.getFamName().equalsIgnoreCase(name)){
+										Guild g = message.getGuild();
+										Brain b = Shmames.getBrains().getBrain(g.getId());
+
+										b.getFamilies().remove(f.getFamID());
+										f.getMemberGuilds().remove(g.getIdLong());
+
+										return "Removed **"+g.getName()+"** from the **"+f.getFamName()+"** Family.";
+									}
+								}
+
+								return Errors.NOT_FOUND;
+							}else{
+								return Errors.NO_PERMISSION_USER;
+							}
+						}
+					}else{
+						return "Use `family remove <familyName>` to remove this server from a Family.\n" +
+								"Use `family remove <familyName> [server number]` to remove another server from a Family.\n" +
+								"_Find the server number using `family view <familyName>`_";
+					}
 				default:
 					return Errors.formatUsage(Errors.WRONG_USAGE, getUsage());
 			}
