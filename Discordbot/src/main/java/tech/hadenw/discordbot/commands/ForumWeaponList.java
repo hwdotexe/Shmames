bad458cb99f7f4691debf8f938e738dc96d6c1e7
Cynicalvoe <hadengw@gmail.com>
2020-06-27 16:35:01 -0600

    Search for a FW

--- Discordbot/src/main/java/tech/hadenw/discordbot/commands/ForumWeaponList.java
+++ Discordbot/src/main/java/tech/hadenw/discordbot/commands/ForumWeaponList.java
@@ -3,10 +3,13 @@ package tech.hadenw.discordbot.commands;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
+import tech.hadenw.discordbot.Errors;
 import tech.hadenw.discordbot.Shmames;
 import tech.hadenw.discordbot.Utils;
 import tech.hadenw.discordbot.storage.Brain;
@@ -21,39 +24,71 @@ public class ForumWeaponList implements ICommand {
 	
 	@Override
 	public String getUsage() {
-		return "fwlist (all)";
+		return "fwlist [all|search <query>]";
 	}
 
 	@Override
 	public String run(String args, User author, Message message) {
+		Matcher m = Pattern.compile("^((all)|(search ([a-z]{3,})))?$", Pattern.CASE_INSENSITIVE).matcher(args);
 		Guild thisG = message.getGuild();
 
-		if(args != null && args.equalsIgnoreCase("all")){
-			StringBuilder sb = new StringBuilder();
+		if(m.find()) {
+			if (m.group(2) != null) {
+				// All
+				StringBuilder sb = new StringBuilder();
 
-			sb.append(buildServerFWList(thisG));
+				sb.append(buildServerFWList(thisG));
 
-			for(String fID : Shmames.getBrains().getBrain(message.getGuild().getId()).getFamilies()){
-				Family f = Shmames.getBrains().getMotherBrain().getFamilyByID(fID);
+				for (String fID : Shmames.getBrains().getBrain(message.getGuild().getId()).getFamilies()) {
+					Family f = Shmames.getBrains().getMotherBrain().getFamilyByID(fID);
 
-				if(f != null){
-					for(long gid : f.getMemberGuilds()){
-						if(gid != thisG.getIdLong()) {
-							Guild g = Shmames.getJDA().getGuildById(gid);
+					if (f != null) {
+						for (long gid : f.getMemberGuilds()) {
+							if (gid != thisG.getIdLong()) {
+								Guild g = Shmames.getJDA().getGuildById(gid);
 
-							if (g != null) {
-								sb.append("\n\n");
-								sb.append(buildServerFWList(g));
+								if (g != null) {
+									sb.append("\n\n");
+									sb.append(buildServerFWList(g));
+								}
 							}
 						}
 					}
 				}
-			}
 
-			return sb.toString();
+				return sb.toString();
+			} else if (m.group(3) != null) {
+				// Searching for a name (g4)
+				String name = m.group(4);
+
+				StringBuilder sb = new StringBuilder();
+
+				sb.append(searchServerList(thisG, name));
+
+				for (String fID : Shmames.getBrains().getBrain(message.getGuild().getId()).getFamilies()) {
+					Family f = Shmames.getBrains().getMotherBrain().getFamilyByID(fID);
+
+					if (f != null) {
+						for (long gid : f.getMemberGuilds()) {
+							if (gid != thisG.getIdLong()) {
+								Guild g = Shmames.getJDA().getGuildById(gid);
+
+								if (g != null) {
+									sb.append("\n\n");
+									sb.append(searchServerList(thisG, name));
+								}
+							}
+						}
+					}
+				}
+
+				return sb.toString();
+			} else {
+				// Just list out this server
+				return buildServerFWList(thisG);
+			}
 		}else{
-			// Just this server.
-			return buildServerFWList(thisG);
+			return Errors.formatUsage(Errors.WRONG_USAGE, getUsage());
 		}
 	}
 
@@ -84,5 +119,20 @@ public class ForumWeaponList implements ICommand {
 		String list = Utils.GenerateList(fwSorted, -1);
 
 		return "**"+g.getName()+"**\n" + (list.length()>2 ? list.substring(2) : "> None Found");
+	}
+
+	private String searchServerList(Guild g, String q){
+		String id = g.getId();
+		HashMap<String, Integer> fwList = new HashMap<String, Integer>();
+
+		for(ForumWeaponObj fws : Shmames.getBrains().getBrain(id).getForumWeapons()) {
+			if(fws.getItemName().contains(q.toLowerCase()))
+				fwList.put(fws.getItemName(), fws.getUses());
+		}
+
+		LinkedHashMap<String, Integer> fwSorted = Utils.sortHashMap(fwList);
+		String list = Utils.GenerateList(fwSorted, -1);
+
+		return "**"+g.getName()+"**\n" + (list.length()>2 ? list.substring(2) : "> No Results");
 	}
 }
