fee57b39ff95712049fc68e7e89d58bd61838acf
Haden Watne <haden@hadenwatne.com>
2021-10-19 20:50:17 -0600

    v1.1.29
    - Fix the roll readout triggering a markdown syntax. [xx](yy) looks like a Markdown URL, which Discord was parsing.

--- src/main/java/com/hadenwatne/shmames/commands/Roll.java
+++ src/main/java/com/hadenwatne/shmames/commands/Roll.java
@@ -186,9 +186,9 @@ public class Roll implements ICommand {
 					}
 
 					diceSB.append("]");
-					diceSB.append("(");
+					diceSB.append("{");
 					diceSB.append(subTotal);
-					diceSB.append(")");
+					diceSB.append("}");
 				} else {
 					// Flat modifier.
 					subTotal += diceSizeOrFlat;
